30025ccb5b5d288949f6dc37c4def021686995fa
Manuel Schoebel <mail@manuelschoebel.de>
2014-04-09 11:46:31 +0200

    reactive sorting, data context and so on

--- ws.js
+++ ws.js
@@ -1,26 +1,26 @@
+Players = new Meteor.Collection('players');
+
 if (Meteor.isClient) {
-  Session.setDefault('name', 'Anonymus');
-  Session.setDefault('color', 'black');
 
-  Template.hello.greeting = function () {
-    return 'Hello ' + Session.get('greet');
-  };
-
-  Template.hello.color = function() {
-    return Session.get('color');
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.playerList.helpers({
+    players: function() {
+      return Players.find({}, {sort: {points: -1}});
     }
   });
+
+  Template.playerTableRow.events({
+    'click button': function(evt, tpl) {
+      Players.update({_id: this._id}, {$inc: {points: 1}});
+    }
+  })
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    Players.remove({});
+    Players.insert({name: 'Manuel', points: 5});
+    Players.insert({name: 'Nina', points: 8});
     // code to run on server at startup
   });
 }
